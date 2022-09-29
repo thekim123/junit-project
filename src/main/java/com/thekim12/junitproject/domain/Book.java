c8f85c930024dbfceb0729ebf33ad8db6a86a4f3
thekim123 <tacdo12@naver.com>
2022-09-29 15:01:38 +0900

    Book 엔티티 생성 완료

--- src/main/java/com/thekim12/junitproject/domain/Book.java
+++ src/main/java/com/thekim12/junitproject/domain/Book.java
@@ -1,4 +1,30 @@
 package com.thekim12.junitproject.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
